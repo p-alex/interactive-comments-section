fc6cacd68ee06c335b8973616b6c1a16fcdb0ea4
p-alex <alexey9021@gmail.com>
2021-12-28 23:15:26 +0200

    Add: title atribute to footer link

--- src/footer.ts
+++ src/footer.ts
@@ -2,7 +2,7 @@ export const appendFooter = (): void => {
   const footer = document.createElement("footer") as Element;
   footer.classList.add("footer");
   footer.innerHTML = `
-      <p>Developed by <a href="https://github.com/p-alex" rel="noopener" target="_blank">Alex Daniel</a>.</p>
+      <p>Developed by <a href="https://github.com/p-alex" rel="noopener" target="_blank" title="My Github Profile">Alex Daniel</a>.</p>
     `;
   document.body.appendChild(footer);
 };
